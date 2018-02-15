5a06a35fe5f7094f771a5931fa79b2854edbb477
dominguis <nacho.dominguis@gmail.com>
2018-02-15 10:20:13 +0100

    first mini commits

--- init.sh
+++ init.sh
@@ -8,28 +8,30 @@ mkdir -p www
 mkdir -p platforms
 
 # Copy non-free asset files
-if [ -d ./nonfree ]
-then
-    cp -R ./nonfree/* ./src;
-elif [ -e ./nonfree.tar.gz ]
-then
-    tar xzf ./nonfree.tar.gz;
-    cp -R ./nonfree/* ./src;
-else
-    echo "Please put the file 'nonfree.tar.gz' or the extracted directory 'nonfree' into the main folder and try again";
-    exit;
-fi
+# if [ -d ./nonfree ]
+# then
+#     cp -R ./nonfree/* ./src;
+# elif [ -e ./nonfree.tar.gz ]
+# then
+#     tar xzf ./nonfree.tar.gz;
+#     cp -R ./nonfree/* ./src;
+# else
+#     echo "Please put the file 'nonfree.tar.gz' or the extracted directory 'nonfree' into the main folder and try again";
+#     exit;
+# fi
 
 # Install the needed cordova plugins
-cordova plugin add 	org.apache.cordova.camera  \
-                    org.apache.cordova.device  \
-                    org.apache.cordova.media   \
-                    org.apache.cordova.file    \
-                    org.apache.cordova.console
+cordova plugin add 	cordova-plugin-camera  \
+                    cordova-plugin-device  \
+                    cordova-plugin-media   \
+                    cordova-plugin-file    \
+                    cordova-plugin-console
 
 # Add the platforms
 cordova platform add android
-cordova platform add ios
+# cordova platform add ios
+# cordova platform add windows
+cordova platform add browser
 
 echo "";
 echo "Now put the files 'ant.properties' and 'my-release-key.keystore' into the ./overlay/android directory";
